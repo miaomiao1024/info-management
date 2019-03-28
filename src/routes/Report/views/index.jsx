f3114007f5a6ee9bdc76856611746ba299bbbf90
liumiaomiao_i <liumiaomiao_i@didichuxing.com>
2019-03-29 00:45:11 +0800

    add:界面制作

--- src/routes/Report/views/index.jsx
+++ src/routes/Report/views/index.jsx
@@ -26,65 +26,68 @@ class sceneRoport extends Component {
     return (
       <div className="report-page">
         <Row>
-          <Col span='3' style={{fontSize:'18px',fontWeight:800,paddingBottom:'40px'}}>
+          <Col span='3' style={{fontSize:'18px',fontWeight:800,paddingBottom:'30px'}}>
             大屏展示
           </Col>
         </Row>
-        <Row type="flex" justify="start">
-          <Col span={FORM_ITEM_SPAN}>
-            <FormItem
-              label="场景名称"
-              {...formItemLayout}
-             >
-              {getFieldDecorator('name')(
-                <Input placeholder="请输入场景名称" />,
-              )}
-             
-            </FormItem>
-          </Col>
-          <Col>
-          （ 场景名称显示8个字以内 ）
-          </Col>
-        </Row>
-        <Row>
-          <Col span={FORM_ITEM_SPAN}>
-            <FormItem
-              label="选择布局"
-              {...formItemLayout}
-             >
-              <Col span='4'><Icon type="laptop" style={{ fontSize: 24,}}/></Col>
-              <Col span='4'><Icon type="appstore-o" style={{ fontSize: 24}}/></Col>
-              <Col span='4'><Icon type="layout" style={{ fontSize: 24, color: '#08c'}}/></Col>
-              <Col span='4'><Icon type="exception" style={{ fontSize: 24}}/></Col>  
-            </FormItem>
-          </Col>
-          
-        </Row>
-        <Row>
-          <Col span={FORM_ITEM_SPAN}>
-            <FormItem
-              label="关联画面"
-              {...formItemLayout}
-             >
-              <Col span='3'><Button type="primary" size='small'>GIS</Button></Col>
-              <Col span='3'><Button type="primary" size='small'>预案</Button></Col>
-              <Col span='3'><Button type="primary" size='small'>告警</Button></Col>
-              <Col span='3'><Button type="primary" size='small'>视频</Button></Col>
-              <Col span='3'><Button type="primary" size='small'>统计</Button></Col>
-              <Col span='9'>拖拽或选中区域点击</Col>
-            </FormItem>
-          </Col>
-          
-        </Row>
-        <Row type="flex" justify="center">
-          <Col span='19'>
-            
+        <div className='search-and-select'>
+          <Row type="flex" justify="start">
+            <Col span={FORM_ITEM_SPAN}>
+              <FormItem
+                label="场景名称"
+                {...formItemLayout}
+              >
+                {getFieldDecorator('name')(
+                  <Input placeholder="请输入场景名称" />,
+                )}
+              
+              </FormItem>
+            </Col>
+            <Col>
+            （ 场景名称显示8个字以内 ）
+            </Col>
+          </Row>
+          <Row>
+            <Col span={FORM_ITEM_SPAN}>
+              <FormItem
+                label="选择布局"
+                {...formItemLayout}
+              >
+                <Col span='4'><Icon type="laptop" style={{ fontSize: 24,}}/></Col>
+                <Col span='4'><Icon type="appstore-o" style={{ fontSize: 24}}/></Col>
+                <Col span='4'><Icon type="layout" style={{ fontSize: 24, color: '#08c'}}/></Col>
+                <Col span='4'><Icon type="exception" style={{ fontSize: 24}}/></Col>  
+              </FormItem>
             </Col>
             
+          </Row>
+          <Row>
+            <Col span={FORM_ITEM_SPAN}>
+              <FormItem
+                label="关联画面"
+                {...formItemLayout}
+              >
+                <Col span='3'><Button type="primary" size='small'>GIS</Button></Col>
+                <Col span='3'><Button type="primary" size='small'>预案</Button></Col>
+                <Col span='3'><Button type="primary" size='small'>告警</Button></Col>
+                <Col span='3'><Button type="primary" size='small'>视频</Button></Col>
+                <Col span='3'><Button type="primary" size='small'>统计</Button></Col>
+                <Col span='9'>拖拽或选中区域点击</Col>
+              </FormItem>
+            </Col>
             
-          
-          
-        </Row>
+          </Row>
+          <Row type="flex" justify="center">
+            <Col span='19'>
+              
+              </Col>
+              
+              
+            
+            
+          </Row>
+        </div>
+        
 
       </div>
         
